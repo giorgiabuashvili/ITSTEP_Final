75612875b12a04c933701e57bafee6dfe93f28cb
giorgiabuashvili <abuashvilig0719@gmail.com>
2023-05-06 19:54:16 +0400

    created route system for login/register

--- BachiraApp/src/app/header/header.component.ts
+++ BachiraApp/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-header',
@@ -14,6 +15,8 @@ export class HeaderComponent {
     "Brands",
     "Pricing"
   ]
+
+  constructor(public appComponent: AppComponent) {}
 
   SetCorrentNavLink(i: number) {
     this.MarkedNavLink = i;
